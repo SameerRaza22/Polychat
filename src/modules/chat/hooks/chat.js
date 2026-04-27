91752149147c06e2cf4c737ad7eab2fb82080234
Sameer Raza <sameer10raza@gmail.com>
2026-04-27 19:30:38 +0530

    Chat sidebar

--- src/modules/chat/hooks/chat.js
+++ src/modules/chat/hooks/chat.js
@@ -1,7 +1,8 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { createChatWithMessage } from "@/modules/chat/components/actions";
+import { createChatWithMessage, deleteChat } from "@/modules/chat/components/actions";
 import { error } from "better-auth/api";
+import { toast } from "sonner";
 
 export const useCreateChat = () => {
     const queryClient = useQueryClient();
@@ -21,6 +22,24 @@ export const useCreateChat = () => {
         onError: (error) => {
             console.error("Create chat error:", error);
             toast.error("Failed to create chat")
+        }
+    });
+};
+
+
+export const useDeleteChat = (chatId) => {
+    const queryClient = useQueryClient();
+
+    const router = useRouter();
+
+    return useMutation({
+        mutationFn: () => deleteChat(chatId),
+        onSuccess: () => {
+            queryClient.invalidateQueries(["chats"]);
+        },
+
+        onError: () => {
+            toast.error("Failed to delete chat");
         }
     });
 };
